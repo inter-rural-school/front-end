daa9f468aaa3439aad59a7c94eeac43681fb26e0
WStrellis <wstrellis@gmail.com>
2019-09-23 13:41:39 -0400

    made layout component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
 
-import Login from './Components/Login';
+import Login from './Pages/Login';
+import Home from './Pages/home.page'
 
 function App() {
   return (
     <div className="App">
+      <Route exact path="/" component={ Home } />
       <Route path="/login" component={Login} />
     </div>
   );
